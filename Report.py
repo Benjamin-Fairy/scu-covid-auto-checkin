a8d0f27cb03fdcbac911d3fc7cf6283d7c632b8d
Benjamin_Fairy <facionia@vip.qq.com>
2022-04-14 16:05:18 +0800

    Update Report.py

--- Report.py
+++ Report.py
@@ -6,11 +6,11 @@ body = os.environ['BODY'] #邮件正文
 filename = "" #要发送的附件
 
 yag = yagmail.SMTP(
-user=os.environ['USR'],#要发送邮件的邮箱，可以自己发送给自己
+user="testfromfairyland@163.com",#要发送邮件的邮箱，可以自己发送给自己
 password=os.environ['PWD'],#授权码
 host = 'smtp.163.com')
 
 yag.send(
     to=receiver,
-    subject=os.environ['TITLE'],#邮件标题
+    subject="打卡结果",#邮件标题
     contents=body)
