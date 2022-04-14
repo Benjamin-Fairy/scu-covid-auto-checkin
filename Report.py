3da52032d46453bab777ac7ed8748c8d15075a39
Benjamin_Fairy <facionia@vip.qq.com>
2022-04-14 16:52:13 +0800

    Update Report.py

--- Report.py
+++ Report.py
@@ -7,7 +7,7 @@ filename = "" #要发送的附件
 
 yag = yagmail.SMTP(
 user="testfromfairyland@163.com",#要发送邮件的邮箱，可以自己发送给自己
-password= "XHHBBSMBFZDVANPO",#授权码
+password= os.environ["PWD"],#授权码
 host = 'smtp.163.com')
 
 yag.send(
