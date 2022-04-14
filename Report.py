b9135639cfb2e0b1a304cef666b8fd3e92dc6327
Benjamin_Fairy <facionia@vip.qq.com>
2022-04-14 16:46:44 +0800

    Update Report.py

--- Report.py
+++ Report.py
@@ -1,22 +1,17 @@
 import yagmail
 import os
 
-f=os.popen("python3 checkin.py",'r')
-res=f.readlines()
-f.close()
-
-
-
-receiver = os.environ['RCV'] #要接收邮件的邮箱
-body = res #邮件正文
+receiver = “facionia@outlook.com” #要接收邮件的邮箱
+body = “test” #邮件正文
 filename = "" #要发送的附件
 
 yag = yagmail.SMTP(
 user="testfromfairyland@163.com",#要发送邮件的邮箱，可以自己发送给自己
-password=os.environ['PWD'],#授权码
+password= os.environ['PWD'],#授权码
 host = 'smtp.163.com')
 
 yag.send(
     to=receiver,
     subject="打卡结果",#邮件标题
     contents=body)
+
